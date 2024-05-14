2d812450d87b3d04e54f4d5dacbfaebb7369fbaa
Lucas Florencio <152495424+LucasFlorencioA@users.noreply.github.com>
2024-05-14 10:05:47 -0300

    ok

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const btnAbrir = document.getElementById('btn-abrir-menu')
 const btnsair = document.getElementById('sair')
 const foraDaTela = document.getElementById('mobile')
 
-btnAbrir.addEventListener('click', (A)=>{
+btnAbrir.addEventListener('click', (A) => {
     A.preventDefault()
     foraDaTela.classList.add('active')
 })
 
-btnsair.addEventListener('click', (A)=>{
+btnsair.addEventListener('click', (A) => {
     A.preventDefault()
     foraDaTela.classList.remove('active')
 })
+
+
+const transform = new IntersectionObserver((arguments) => {
+    arguments.forEach((e) => {
+        if (e.isIntersecting) {
+            e.target.classList.add("show")
+        }
+    })
+})
+
+const anime = document.querySelectorAll(".hidden")
+anime.forEach((t) => transform.observe(t))
