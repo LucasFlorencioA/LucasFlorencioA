810739c7bf3007365ea7cb03ad11f23f41b7c3a7
Lucas Florencio <152495424+LucasFlorencioA@users.noreply.github.com>
2024-08-02 19:53:36 -0300

    Update aula02.py

--- APRENDENDO_PROGRAMACAO/Lucas/aula02.py
+++ APRENDENDO_PROGRAMACAO/Lucas/aula02.py
@@ -1,2 +1,20 @@
-frutas = {"maçã", "banana", "cereja", "maçã"}
-print(frutas)
+competicao = [
+    ('Equipe 1', [70, 85, 98, 89]),
+    ('Equipe 2', [75, 78, 75, 95]),
+    ('Equipe 3', [90, 75, 99, 84])
+]
+
+# Converte a lista de tuplas em um dicionário
+competicao_dict = dict(competicao)
+
+# Calcula a média dos pontos para cada equipe
+medias = [(nome, sum(pontos)/ len(pontos))for nome, pontos in competicao_dict.items()]
+
+
+# Ordena a lista de médias em ordem decrescente
+# O método sort() modifica a lista no local e retorna None
+# Portanto, você precisa usar a função sorted() se quiser criar uma nova lista ordenada
+oredem_das_medias = sorted(medias, key=lambda x: x[1], reverse=True)
+
+# Exibe a lista ordenada
+print(oredem_das_medias)
